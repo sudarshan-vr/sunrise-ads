60e86be98713157367f969dbe487f4cc04b54943
Sudarshan VR <sudarshanvr23@gmail.com>
2026-01-19 23:33:09 +0530

    chore: remove .next directory from git tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kGIunSYOicNFqmPUO2YupG/ykhcLFn2jETxpm5PwEhI=\"\n}"
--- .next/static/chunks/src_app_services_page_tsx_5f2ed94d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_b95e17b4._.js",
-  "static/chunks/src_components_Services_index_tsx_dac42453._.js"
-],
-    source: "dynamic"
-});
